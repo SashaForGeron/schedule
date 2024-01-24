1a9c1c6d90e2da3c1ee49347a882e9049a0a01ea
SashaForGeron <149246979+SashaForGeron@users.noreply.github.com>
2024-01-24 15:41:30 +0600

    Add files via upload

---
+++ homeWork9/app/src/main/java/com/example/homework9/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.homework9;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private String titles[] = {"Январь", "Февраль", "Март", "Апрель", "Май", "Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView listView = findViewById(R.id.listView);
+        listView.setAdapter(new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, titles));
+        listView.setTextFilterEnabled(true);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View v, int position, long id) {
+                Intent intent = new Intent();
+                intent.setClass(MainActivity.this,DetailActivity.class);
+                intent.putExtra("title",position);
+                startActivity(intent);
+            }
+        });
+    }
+}
